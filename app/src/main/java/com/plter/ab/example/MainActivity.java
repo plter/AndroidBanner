3b45d3281d240a52a23bf654ad92191819854816
yunp <xtiqin@163.com>
2018-06-25 16:29:17 +0800

    ok

--- app/src/main/java/com/plter/ab/example/MainActivity.java
+++ app/src/main/java/com/plter/ab/example/MainActivity.java
@@ -1,10 +1,9 @@
 package com.plter.ab.example;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
 
 import com.plter.lib.ab.AndroidBanner;
 
